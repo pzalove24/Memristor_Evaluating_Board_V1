cd3228d5387fb7b0300373ea56b43f7af916a75b
Peeratchai Kleebbua <p-ratchai@hotmail.com>
2023-04-19 18:13:02 +0700

    switch

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,34 @@
 #include <Arduino.h>
 
+const int inPin1 = 7;
+const int inPin2 = 6;
+const int inPin3 = 5;
+const int inPin4 = 4;
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(inPin1, OUTPUT);
+  pinMode(inPin2, OUTPUT);
+  pinMode(inPin3, OUTPUT);
+  pinMode(inPin4, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(inPin1, HIGH);
+  delay(5000);
+  digitalWrite(inPin1, LOW);
+  delay(5000);
+  digitalWrite(inPin2, HIGH);
+  delay(5000);
+  digitalWrite(inPin2, LOW);
+  delay(5000);
+  digitalWrite(inPin3, HIGH);
+  delay(5000);
+  digitalWrite(inPin3, LOW);
+  delay(5000);
+  digitalWrite(inPin4, HIGH);
+  delay(5000);
+  digitalWrite(inPin4, LOW);
+  delay(5000);
 }
