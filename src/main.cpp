7de5bbdf770a7a42499b4128e7f2a76cfdc6babb
Peeratchai Kleebbua <p-ratchai@hotmail.com>
2023-04-19 18:43:09 +0700

    serial

--- src/main.cpp
+++ src/main.cpp
@@ -11,24 +11,33 @@ void setup() {
   pinMode(inPin2, OUTPUT);
   pinMode(inPin3, OUTPUT);
   pinMode(inPin4, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("1Mohm : 1uA");
   digitalWrite(inPin1, HIGH);
   delay(5000);
+  Serial.println("Shutdown 1");
   digitalWrite(inPin1, LOW);
   delay(5000);
+  Serial.println("100Kohm : 10uA");
   digitalWrite(inPin2, HIGH);
   delay(5000);
+  Serial.println("Shutdown 2");
   digitalWrite(inPin2, LOW);
   delay(5000);
+  Serial.println("10Kohm : 100uA");
   digitalWrite(inPin3, HIGH);
   delay(5000);
+  Serial.println("Shutdown 3");
   digitalWrite(inPin3, LOW);
   delay(5000);
+  Serial.println("1Kohm : 1mA");
   digitalWrite(inPin4, HIGH);
   delay(5000);
+  Serial.println("Shutdown 4");
   digitalWrite(inPin4, LOW);
   delay(5000);
 }
