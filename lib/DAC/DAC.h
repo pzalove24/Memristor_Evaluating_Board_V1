f998c5636763401cc8a1649cdfcefefa45829c96
Peeratchai Kleebbua <p-ratchai@hotmail.com>
2023-05-07 20:47:32 +0700

    dac_add

--- lib/DAC/DAC.h
+++ lib/DAC/DAC.h
@@ -11,7 +11,7 @@ class DAC_bias {
         Adafruit_MCP4725 dac;
         void DAC_setup();
         void Test_Sequence_Triangular_Wave();
-        void pulse_bias(float PositiveVoltage, float NegativeVoltage, int PositivePulseNumber, int NegativePulseNumber, float DelayRead_us, float DelayTime_ms, bool continuoes = false);
+        void pulse_bias(float PositiveVoltage, float NegativeVoltage, float ReadVoltage = 0.1, float PulseWidth_us = 100, int PositivePulseNumber=10, int NegativePulseNumber=10, float DelayRead_us = 50, float DelayTime_ms = 100, bool continuoes = false);
         void pulse_IV(float voltage_start, float voltage_end, float Vstep, int PositivePulseNumber, int NegativePulseNumber, float DelayTime_ms, bool continuoes = false);
 };
 
