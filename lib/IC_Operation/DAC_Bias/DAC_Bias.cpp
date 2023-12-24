d8c85c12247a5152d2d7746531477a7b478ffa35
Peeratchai Kleebbua <p-ratchai@hotmail.com>
2023-12-25 05:29:52 +0700

    bias

--- lib/IC_Operation/DAC_Bias/DAC_Bias.cpp
+++ lib/IC_Operation/DAC_Bias/DAC_Bias.cpp
@@ -40,8 +40,8 @@ void DAC_Bias::Test_Sequence_Triangular_Wave()
   }
 }
 
-void DAC_Bias::pulse_bias(float PositiveVoltage, float NegativeVoltage, float ReadVoltage = 0.1, int PulseWidth_us = 100, int PositivePulseNumber = 0, int NegativePulseNumber = 0,
-                          int DelayRead_us = 50, int DelayTime_ms = 100, bool continuoes = false)
+void DAC_Bias::test_pulse_bias(float PositiveVoltage, float NegativeVoltage, float ReadVoltage = 0.1, int PulseWidth_us = 100, int PositivePulseNumber = 0, int NegativePulseNumber = 0,
+                          int DelayRead_us = 50, int DelayTime_us = 100, bool continuoes = false)
 {
   uint32_t PositiveVoltage_counter = (PositiveVoltage * 4095) / 5;
   uint32_t NegativeVoltage_counter = (NegativeVoltage * 4095) / 5;
@@ -59,7 +59,7 @@ void DAC_Bias::pulse_bias(float PositiveVoltage, float NegativeVoltage, float Re
       dac.setVoltage(ReadVoltage_counter, false); // read
       delayMicroseconds(PulseWidth_us);
       dac.setVoltage(0, false);
-      delay(DelayTime_ms);
+      delay(DelayTime_us);
     }
     Serial.println("Generate Negative Pulse Number");
     for (int i; i < (PositivePulseNumber + NegativePulseNumber); i++)
@@ -71,7 +71,7 @@ void DAC_Bias::pulse_bias(float PositiveVoltage, float NegativeVoltage, float Re
       dac.setVoltage(ReadVoltage_counter, false); // read
       delayMicroseconds(PulseWidth_us);
       dac.setVoltage(0, false);
-      delay(DelayTime_ms);
+      delay(DelayTime_us);
     }
     Serial.println("Done Generating in this loop");
   } while (continuoes);
@@ -110,4 +110,139 @@ void DAC_Bias::transient_IV(float voltage_start = 0, float voltage_end = 5, floa
       // }
     } while (continuoes);
   }
+}
+
+void DAC_Bias::sweep_bias_IV(float positive_voltage = 8, float negative_voltage = -8, float Vstep = 0.0015, int voltage_width_us = 100, int repeat_cycle = 1)
+{
+  uint32_t counter_start_positive = (0 * 4095) / 8;
+  uint32_t counter_end_positive = (positive_voltage * 4095) / 8;
+  uint32_t counter_increase_positive = (Vstep * 4095) / 8;
+
+  uint32_t counter_start_negative = (0 * 4095) / 8;
+  uint32_t counter_end_negative = (negative_voltage * 4095) / 8;
+  uint32_t counter_increase_negative = (Vstep * 4095) / 8;
+  // Run through the full 12-bit scale for a triangle wave
+  // 0.0012 V = 1 in 4095 resolution as Vstep at 5V
+  int current_cycle = 0;
+  if (Vstep < 0.0015 || positive_voltage > 8 || negative_voltage < -8)
+  {
+    Serial.println("Vstep is exceed DAC resolution or voltage_set is exceed +-8V");
+  }
+  else
+  {
+    do
+    {
+      // positive sweep 0 => positive
+      for (counter_start_positive; counter_start_positive < 4095; counter_start_positive = counter_start_positive + counter_increase_positive)
+      {
+        dac.setVoltage(counter_start_positive, false);
+        delayMicroseconds(voltage_width_us);
+      }
+      // positive sweep positive => 0
+      for (counter_end_positive; counter_end_positive > 0; counter_end_positive = counter_end_positive - counter_increase_positive)
+      {
+        dac.setVoltage(counter_end_positive, false);
+        delayMicroseconds(voltage_width_us);
+      }
+
+      // negative sweep 0 => negative
+      for (counter_start_negative; counter_start_negative < -4095; counter_start_negative = counter_start_negative - counter_increase_negative)
+      {
+        dac.setVoltage(counter_start_negative, false);
+        delayMicroseconds(voltage_width_us);
+      }
+      // negative sweep negative => 0
+      for (counter_end_negative; counter_end_negative > 0; counter_end_negative = counter_end_negative + counter_increase_negative)
+      {
+        dac.setVoltage(counter_end_negative, false);
+        delayMicroseconds(voltage_width_us);
+      }
+      current_cycle++;
+    } while (current_cycle < repeat_cycle); // repeat time
+  }
+
+//   void DAC_Bias::sweep_bias_IV(float positive_voltage = 8, float negative_voltage = -8, float Vstep = 0.0015, int voltage_width_us = 100, int repeat_cycle = 1)
+// {
+//   const uint32_t positive_increment = static_cast<uint32_t>((Vstep * 4095) / 8);
+//   const uint32_t negative_increment = static_cast<uint32_t>((-Vstep * 4095) / 8);
+
+//   const uint32_t counter_end_positive = static_cast<uint32_t>((positive_voltage * 4095) / 8);
+//   const uint32_t counter_end_negative = static_cast<uint32_t>((negative_voltage * 4095) / 8);
+
+//   // Validate input parameters
+//   if (Vstep < 0.0015 || positive_voltage > 8 || negative_voltage < -8)
+//   {
+//     Serial.println("Vstep exceeds DAC resolution or voltage range exceeds +-8V");
+//     return;
+//   }
+
+//   for (int cycle = 0; cycle < repeat_cycle; ++cycle)
+//   {
+//     // Positive sweep: 0 => positive
+//     for (uint32_t counter = 0; counter < counter_end_positive; counter += positive_increment)
+//     {
+//       dac.setVoltage(counter, false);
+//       delayMicroseconds(voltage_width_us);
+//     }
+
+//     // Positive sweep: positive => 0
+//     for (uint32_t counter = counter_end_positive; counter > 0; counter -= positive_increment)
+//     {
+//       dac.setVoltage(counter, false);
+//       delayMicroseconds(voltage_width_us);
+//     }
+
+//     // Negative sweep: 0 => negative
+//     for (uint32_t counter = 0; counter > counter_end_negative; counter += negative_increment)
+//     {
+//       dac.setVoltage(counter, false);
+//       delayMicroseconds(voltage_width_us);
+//     }
+
+//     // Negative sweep: negative => 0
+//     for (uint32_t counter = counter_end_negative; counter > 0; counter -= negative_increment)
+//     {
+//       dac.setVoltage(counter, false);
+//       delayMicroseconds(voltage_width_us);
+//     }
+//   }
+// }
+
+}
+
+void DAC_Bias::pulse_bias(float PositiveVoltage, float NegativeVoltage, float ReadVoltage = 0.1, int PulseWidth_us = 100, int PositivePulseNumber = 0, int NegativePulseNumber = 0,
+                          int DelayRead_us = 50, int DelayTime_us = 100, bool continuoes = false)
+{
+  uint32_t PositiveVoltage_counter = (PositiveVoltage * 4095) / 5;
+  uint32_t NegativeVoltage_counter = (NegativeVoltage * 4095) / 5;
+  uint32_t ReadVoltage_counter = (ReadVoltage * 4095) / 5;
+
+  do
+  {
+    Serial.println("Generate Positive Pulse Number");
+    for (int i = 0; i < PositivePulseNumber; i++)
+    {
+      dac.setVoltage(PositiveVoltage_counter, false); // bias
+      delayMicroseconds(PulseWidth_us);
+      dac.setVoltage(0, false);
+      delayMicroseconds(DelayRead_us);
+      dac.setVoltage(ReadVoltage_counter, false); // read
+      delayMicroseconds(PulseWidth_us);
+      dac.setVoltage(0, false);
+      delay(DelayTime_us);
+    }
+    Serial.println("Generate Negative Pulse Number");
+    for (int i; i < (PositivePulseNumber + NegativePulseNumber); i++)
+    {
+      dac.setVoltage(NegativeVoltage_counter, false); // bias
+      delayMicroseconds(PulseWidth_us);
+      dac.setVoltage(0, false);
+      delayMicroseconds(DelayRead_us);
+      dac.setVoltage(ReadVoltage_counter, false); // read
+      delayMicroseconds(PulseWidth_us);
+      dac.setVoltage(0, false);
+      delay(DelayTime_us);
+    }
+    Serial.println("Done Generating in this loop");
+  } while (continuoes);
 }
--- lib/IC_Operation/DAC_Bias/DAC_Bias.h
+++ lib/IC_Operation/DAC_Bias/DAC_Bias.h
@@ -24,8 +24,14 @@ public:
     // Getter
 
     void Test_Sequence_Triangular_Wave();
-    void pulse_bias(float PositiveVoltage, float NegativeVoltage, float ReadVoltage = 0.1, int PulseWidth_us = 100, int PositivePulseNumber = 0, int NegativePulseNumber = 0, int DelayRead_us = 50, int DelayTime_ms = 100, bool continuoes = false);
     void transient_IV(float voltage_start = 0, float voltage_end = 5, float Vstep = 0.0015, int DelayTime_ms = 1, bool continuoes = false);
+    void test_pulse_bias(float PositiveVoltage, float NegativeVoltage, float ReadVoltage = 0.1, int PulseWidth_us = 100, int PositivePulseNumber = 0, int NegativePulseNumber = 0, int DelayRead_us = 50, int DelayTime_ms = 100, bool continuoes = false);
+    void pulse_bias(float PositiveVoltage, float NegativeVoltage, float ReadVoltage = 0.1, int PulseWidth_us = 100, int PositivePulseNumber = 0, int NegativePulseNumber = 0, int DelayRead_us = 50, int DelayTime_ms = 100, bool continuoes = false);
+    void sweep_bias_IV(float positive_voltage = 8, float negative_voltage = -8, float Vstep = 0.0015, int voltage_width_us = 100, int repeat_cycle = 1);
+    void conductancePulse(float positive_voltage, float negative_voltage, float read_voltage = 0.1, int PulseWidth_us = 100, int PositivePulseNumber = 0, int NegativePulseNumber = 0, int DelayRead_us = 50, int DelayTime_ms = 100, bool continuoes = false);
+    void enduranceCycle(float positive_voltage, float negative_voltage, float read_voltage = 0.1, int pulseWidth_positive_us = 100, int pulseWidth_negative_us = 100, int pulseWidth_read_us = 100, int interval_us = 50, int repeat = 1);
+    void retention_sweep(float positive_voltage, float negative_voltage, float read_voltage = 0.1, int width_positive_us = 100, int width_negative_us = 100, int width_read_us = 100, int interval_us = 50, int repeat = 1);
+    void retention_pulse(float positive_voltage, float negative_voltage, float read_voltage = 0.1, int pulseWidth_positive_us = 100, int pulseWidth_negative_us = 100, int pulseWidth_read_us = 100, int wait_us = 50, int repeat = 1);
 
     // Abstract
     // Friend
